bc2693bf734cf2e6a4198c916f9729c965ccba4e
Cody Fields (Leeous) <contact@leeous.com>
2025-05-01 21:36:02 -0400

    add interact hook for woodpile, draw empty woodpiles over farmhouse

--- WoodStorage/ModEntry.cs
+++ WoodStorage/ModEntry.cs
@@ -1,39 +1,81 @@
 ﻿using System;
+using System.Reflection.Metadata;
 using Microsoft.Xna.Framework;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
-using StardewModdingAPI.Utilities;
 using StardewValley;
+using StardewValley.GameData.Buildings;
+using StardewValley.GameData.Locations;
+using StardewValley.GameData.Objects;
+using xTile;
 
 namespace WoodStorage
 {
-  /// <summary>The mod entry point.</summary>
+  public static class Constants
+  {
+    public const string WoodpileTileAction = "Leeous.Woodpile_Open";
+  }
+  /// <summary>Mod entry point</summary>
   internal sealed class ModEntry : Mod
   {
-    /*********
-     ** Public methods
-     *********/
-    /// <summary>The mod entry point, called after the mod is first loaded.</summary>
-    /// <param name="helper">Provides simplified APIs for writing mods.</param>
     public override void Entry(IModHelper helper)
     {
-      helper.Events.Input.ButtonPressed += this.OnButtonPressed;
+      // Edit tiles
+      helper.Events.Content.AssetRequested += Content_AssetRequested;
+      // Register interact hook
+      GameLocation.RegisterTileAction(Constants.WoodpileTileAction, OnWoodpileOpen);
     }
-        
-    /*********
-     ** Private methods
-     *********/
-    /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-    /// <param name="sender">The event sender.</param>
-    /// <param name="e">The event data.</param>
-    private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-    {
-      // ignore if player hasn't loaded a save yet
-      if (!Context.IsWorldReady)
-        return;
 
-      // print button presses to the console window
-      this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+    /// <summary>On Woodpile interact</summary>
+    private bool OnWoodpileOpen(GameLocation where, string[] arg2, Farmer who, Point what)
+    {
+      this.Monitor.Log((this.Helper.Input.GetCursorPosition().Tile.ToString()), LogLevel.Trace);
+      return true;
+    }
+
+    /// <summary>
+    /// Replace woodpile tiles
+    /// </summary>
+    private void Content_AssetRequested(object? sender, AssetRequestedEventArgs e)
+    {
+      if (e.NameWithoutLocale.IsEquivalentTo("Data/Buildings"))
+      {
+        e.Edit(asset =>
+        {
+          var data = asset.AsDictionary<string, BuildingData>().Data;
+          var farmhouse = data["Farmhouse"];
+          
+          // Draw empty wood pile over farmhouse tiles
+          farmhouse.DrawLayers.Add(new BuildingDrawLayer()
+          {
+            Id = "Leeous.Woodpile_Empty",
+            Texture = Helper.ModContent.GetInternalAssetName("assets/Woodpile_Empty.png").Name,
+            SourceRect = new()
+            {
+              Width = 64,
+              Height = 64,
+            },
+            SortTileOffset = (float)2,
+            DrawInBackground = false,
+            DrawPosition = new()
+            {
+              X = 0,
+              Y = 66
+            },
+          });
+          
+          farmhouse.ActionTiles.Add(new()
+          {
+            Id = "Leeous.Woodpile_Open",
+            Tile = new()
+            {
+              X = 2,
+              Y = 2
+            },
+            Action = "Leeous.Woodpile_Open"
+          });
+        });
+      }
     }
   }
 }
